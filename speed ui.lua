44397d8ec886be0f39ed81a0a4d0308d91b21c22
M2-HLS-717 <229334500+M5-HLS-717@users.noreply.github.com>
2026-02-07 20:43:40 +0300

    Update speed ui.lua

--- speed ui.lua
+++ speed ui.lua
@@ -131,17 +131,17 @@ local function CloseOpen()
 	end
 
 ScreenGui.Name = "OpenClose"
-    ScreenGui.Parent = RunService:IsStudio() and LocalPlayer.PlayerGui or (gethui() or cloneref(game:GetService("CoreGui")) or game:GetService("CoreGui"))
-    ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ScreenGui.Parent = RunService:IsStudio() and LocalPlayer.PlayerGui or (gethui() or cloneref(game:GetService("CoreGui")) or game:GetService("CoreGui"))
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-    Close_ImageButton.Parent = ScreenGui
-    Close_ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-    Close_ImageButton.BorderColor3 = Color3.fromRGB(255, 0, 0)
-    Close_ImageButton.Position = UDim2.new(0.1021, 0, 0.0743, 0)
-    Close_ImageButton.Size = UDim2.new(0, 59, 0, 49)
-    Close_ImageButton.Image = "rbxassetid://96563775903401"
-    Close_ImageButton.Visible = false
-
+Close_ImageButton.Parent = ScreenGui
+Close_ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Close_ImageButton.BorderColor3 = Color3.fromRGB(0, 150, 255) 
+Close_ImageButton.Position = UDim2.new(0.1021, 0, 0.0743, 0)
+Close_ImageButton.Size = UDim2.new(0, 59, 0, 49)
+Close_ImageButton.Image = "rbxassetid://71789972504071" 
+Close_ImageButton.Visible = false
+	
 	UICorner.Name = "MainCorner"
 	UICorner.CornerRadius = UDim.new(0, 9)
 	UICorner.Parent = Close_ImageButton
